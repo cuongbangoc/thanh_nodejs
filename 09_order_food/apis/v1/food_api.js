b0dd7853a3d315a1095411ad0e238dddeff33766
thanh <thanhbangoc@gmail.com>
2018-05-30 23:58:33 +0700

    update food by ID

--- 09_order_food/apis/v1/food_api.js
+++ 09_order_food/apis/v1/food_api.js
@@ -36,4 +36,71 @@ router.post("/create", function(req, res){
     });
 });
 
+// Update food
+router.put("/update/:id", function(req, res){
+    let params = req.body;
+    let id = req.params.id;
+
+    // if (!params || !params.name){
+    //     return res.status(406).json({
+    //         error_code: 406,
+    //         message: "Missing params"
+    //     });
+    // }
+
+    let food_data = food_repo.findById(id);
+    food_data.then(function(food){
+        if (!food){
+            res.status(404).json({
+                error_code: 404,
+                message: "Not found food by id"
+            });
+        }else{
+            if (!params){
+               res.status(200).json({
+                   code: 200,
+                   message: "Update food success",
+                   food: food
+               });
+            }else{
+                if (params.name){
+                    food.name = params.name;
+                }             
+                if (params.food_category){
+                    food.food_category = params.food_category;
+                }
+                if (params.price){
+                    food.price = params.price;
+                }
+                if (params.image){
+                    food.image = params.image;
+                }
+                if (params.description){
+                    food.description = params.description;
+                }
+                food.save(function(error){
+                    if (error){
+                        logger.error(error);
+                        res.status(500).json({
+                            error_code: 500,
+                            message: "Could not update food"
+                        });
+                    }else{
+                        res.status(200).json({
+                            code: 200,
+                            message: "Update food success",
+                            food: food
+                        });
+                    }
+                });
+            } 
+        }
+    }).catch(function(error){
+        logger.error(error);
+        res.status(500).json({
+            error_code:500,
+            message: "Error update food"
+        });
+    });
+});
 module.exports = router;
--- 09_order_food/repositories/food_repository.js
+++ 09_order_food/repositories/food_repository.js
@@ -6,6 +6,9 @@ var FoodRepository = {
     create: function (food) {
         let new_food = new models.Food(food);
         return new_food.save();
+    },
+    findById: function(id){
+        return models.Food.findOne({_id: id});
     }
 }
 
