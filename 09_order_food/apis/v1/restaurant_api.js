2234f5fc1d992a2949fceaca2d5e7d78c82ff1fd
thanh <thanhbangoc@gmail.com>
2018-06-03 23:16:17 +0700

    Update restaurant

--- 09_order_food/apis/v1/restaurant_api.js
+++ 09_order_food/apis/v1/restaurant_api.js
@@ -39,4 +39,70 @@ router.post("/create", function(req, res){
       });
 });
 
+
+// Update restaurant
+router.put("/update/:id", function(req, res){
+      let id = req.params.id;
+      let params = req.body;
+
+      if (!id){
+            return res.status(406).json({
+                  error_code: 406,
+                  message: "id do not exist"
+            });
+      }
+      let restaurant_data = restaurant_repo.findById(id);
+      restaurant_data.then(function(restaurant){
+            if (!restaurant){
+                  res.status(404).json({
+                        error_code: 404,
+                        message: "Not found restaurant by id"
+                  });
+            }else{
+                 if (!params){
+                       return res.status(200).json({
+                             code: 200,
+                             message: "update restaurant success",
+                             data: restaurant
+                       });
+                 }else{
+                       if (params.name){
+                             restaurant.name = params.name;
+                       }
+                       if (params.address){
+                             restaurant.address = params.address;
+                       }
+                       if (params.rating){
+                             restaurant.rating = params.rating;
+                       }
+                       if (params.image){
+                             restaurant.image = params.image;
+                       }
+                       if (params.food){
+                             restaurant.food = params.food;
+                       }
+                       if (params.description){
+                             restaurant.description = params.description;
+                       }
+                       restaurant.save(function(error){
+                             if (error){
+                                   logger.error(error);
+                             }else{
+                                    res.status(200).json({
+                                          code: 200,
+                                          message: "update restaurant success",
+                                          data: restaurant
+                                    });
+                             }                      
+                       });
+                 }  
+            }
+      }).catch(function(error){
+            logger.error(error);
+            res.status(500).json({
+                  error_code: 500,
+                  message: "Error update restaurant"
+            });
+      });
+});
 module.exports = router;
--- 09_order_food/repositories/restaurant_reposiroty.js
+++ 09_order_food/repositories/restaurant_reposiroty.js
@@ -6,6 +6,9 @@ var RestauranrRepository = {
     create: function (restaurant){
         let new_restaurant = new models.Restaurant(restaurant);
         return new_restaurant.save();
+    },
+    findById: function (id){
+        return models.Restaurant.findOne({_id: id});
     }
 }
 
